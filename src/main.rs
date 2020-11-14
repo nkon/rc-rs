8c61ecc4fd03aa7ff84c8109ceee3fa7fe729554
kondou nobuhiro <kondou.nobuhiro@gmail.com>
2020-11-14 14:17:09 -0600

    use enum Token.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
-fn num(s: String) -> i64 {
-    s.parse().unwrap()
+#[derive(Debug, Clone)]
+enum Token {
+    Num(i64),
+}
+
+fn num(s: String) -> Token {
+    Token::Num(s.parse().unwrap())
 }
 
 fn main() {
     println!("Hello, world!");
-    println!("1 -> {}", num("1".to_string()));
-    println!("0 -> {}", num("0".to_string()));
-    println!("-1 -> {}", num("-1".to_string()));
-    println!("9223372036854775807 -> {}", num("9223372036854775807".to_string()));
-    println!("-9223372036854775808 -> {}", num("-9223372036854775808".to_string()));
+    println!("1 -> {:?}", num("1".to_string()));
+    println!("0 -> {:?}", num("0".to_string()));
+    println!("-1 -> {:?}", num("-1".to_string()));
+    println!(
+        "9223372036854775807 -> {:?}",
+        num("9223372036854775807".to_string())
+    );
+    println!(
+        "-9223372036854775808 -> {:?}",
+        num("-9223372036854775808".to_string())
+    );
 }
