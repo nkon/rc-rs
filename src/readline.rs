575f31c01c4c8c3a3f3dd354d27ba07fdffb3da7
kondou nobuhiro <kondou.nobuhiro@gmail.com>
2020-11-22 18:00:43 -0600

    use termion to handle raw terminal.

--- src/readline.rs
+++ src/readline.rs
@@ -1,26 +1,70 @@
-use std::io;
-use std::io::Write;
+use std::io::{stdin, stdout, Write};
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+use termion::*;
 
 use super::*;
 
 pub fn readline(env: &mut Env) -> String {
     let mut line = String::new();
-    loop {
-        print!("rc> ");
-        io::stdout().flush().unwrap();
-        io::stdin()
-            .read_line(&mut line)
-            .expect("Failed to read line");
-        // println!("{}", line);
-        match lexer(line.clone()) {
-            Ok(v) => {
-                let node = parse(env, &v);
-                println!("{:?}", eval(env, &node));
-            }
-            Err(e) => {
-                println!("{}", e);
-            }
+    let mut history: Vec<String> = Vec::new();
+
+    // goto raw mode
+    let stdin = stdin();
+    let mut stdout = stdout().into_raw_mode().unwrap();
+    stdout.flush().unwrap();
+
+    println!("Ctrl-c to exit");
+    write!(stdout, "{}", cursor::Left(500)).unwrap();
+    write!(stdout, "rc> ").unwrap();
+    stdout.flush().unwrap();
+
+    for c in stdin.keys() {
+        match c {
+            Ok(event::Key::Ctrl('c')) => break,
+            Ok(event::Key::Char(c)) => match c {
+                '\n' => {
+                    println!();
+                    history.push(line.clone());                    
+                    write!(stdout, "{}", cursor::Left(500)).unwrap();
+                    match lexer(line.clone()) {
+                        Ok(v) => {
+                            let node = parse(env, &v);
+                            let result = eval(env, &node);
+                            match result.ty {
+                                NodeType::Num => {
+                                    write!(stdout, "{}", style::Bold).unwrap();
+                                    println!("{}", result.value);
+                                    write!(stdout, "{}", style::Reset).unwrap();
+                                }
+                                NodeType::FNum => {
+                                    write!(stdout, "{}", style::Bold).unwrap();
+                                    println!("{}", result.fvalue);
+                                    write!(stdout, "{}", style::Reset).unwrap();
+                                }
+                                _ => {
+                                    println!("eval eror");
+                                }
+                            }
+                        }
+                        Err(e) => {
+                            println!("{}", e);
+                        }
+                    }
+                    line.clear();
+                    println!();
+                    write!(stdout, "{}", cursor::Left(500)).unwrap();
+                    write!(stdout, "rc> ").unwrap();
+                    stdout.flush().unwrap();
+                }
+                _ => {
+                    print!("{}", c);
+                    line.push(c);
+                    stdout.flush().unwrap();
+                }
+            },
+            _ => {}
         }
-        line.clear();
     }
+    String::new()
 }
