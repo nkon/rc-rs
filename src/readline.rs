876aac475b01990ba24ba74c8380e7d7952bcc39
kondou nobuhiro <kondou.nobuhiro@gmail.com>
2020-11-22 18:18:17 -0600

    Impliment BACKSPACE in readline.rs

--- src/readline.rs
+++ src/readline.rs
@@ -7,6 +7,7 @@ use super::*;
 
 pub fn readline(env: &mut Env) -> String {
     let mut line = String::new();
+    let mut cur_x = 0;
     let mut history: Vec<String> = Vec::new();
 
     // goto raw mode
@@ -14,7 +15,7 @@ pub fn readline(env: &mut Env) -> String {
     let mut stdout = stdout().into_raw_mode().unwrap();
     stdout.flush().unwrap();
 
-    println!("Ctrl-c to exit");
+    writeln!(stdout, "Ctrl-c to exit").unwrap();
     write!(stdout, "{}", cursor::Left(500)).unwrap();
     write!(stdout, "rc> ").unwrap();
     stdout.flush().unwrap();
@@ -22,10 +23,16 @@ pub fn readline(env: &mut Env) -> String {
     for c in stdin.keys() {
         match c {
             Ok(event::Key::Ctrl('c')) => break,
+            Ok(event::Key::Backspace) => {
+                line.pop();
+                write!(stdout, "{}{}", clear::CurrentLine, cursor::Left(500)).unwrap();
+                write!(stdout, "rc> {}", line).unwrap();
+                stdout.flush().unwrap();
+            }
             Ok(event::Key::Char(c)) => match c {
                 '\n' => {
-                    println!();
-                    history.push(line.clone());                    
+                    history.push(line.clone());
+                    writeln!(stdout, "").unwrap();
                     write!(stdout, "{}", cursor::Left(500)).unwrap();
                     match lexer(line.clone()) {
                         Ok(v) => {
@@ -34,32 +41,33 @@ pub fn readline(env: &mut Env) -> String {
                             match result.ty {
                                 NodeType::Num => {
                                     write!(stdout, "{}", style::Bold).unwrap();
-                                    println!("{}", result.value);
+                                    writeln!(stdout, "{}", result.value).unwrap();
                                     write!(stdout, "{}", style::Reset).unwrap();
                                 }
                                 NodeType::FNum => {
                                     write!(stdout, "{}", style::Bold).unwrap();
-                                    println!("{}", result.fvalue);
+                                    writeln!(stdout, "{}", result.fvalue).unwrap();
                                     write!(stdout, "{}", style::Reset).unwrap();
                                 }
                                 _ => {
-                                    println!("eval eror");
+                                    writeln!(stdout, "eval eror").unwrap();
                                 }
                             }
                         }
                         Err(e) => {
-                            println!("{}", e);
+                            writeln!(stdout, "{}", e).unwrap();
                         }
                     }
                     line.clear();
-                    println!();
+                    write!(stdout, "").unwrap();
                     write!(stdout, "{}", cursor::Left(500)).unwrap();
                     write!(stdout, "rc> ").unwrap();
                     stdout.flush().unwrap();
                 }
                 _ => {
-                    print!("{}", c);
+                    write!(stdout, "{}", c).unwrap();
                     line.push(c);
+                    cur_x += 1;
                     stdout.flush().unwrap();
                 }
             },
