71b33dd038e1666afa2d56ffbf33a1f2b11ef612
kondou nobuhiro <kondou.nobuhiro@gmail.com>
2020-11-22 23:18:53 -0600

    refactoring: lexer.rs test.

--- src/lexer.rs
+++ src/lexer.rs
@@ -261,171 +261,69 @@ pub fn lexer(s: String) -> Result<Vec<Token>, String> {
 mod tests {
     use super::*;
 
+    fn s2v(str: &str) -> Vec<char> {
+        str.chars().collect::<Vec<char>>()
+    }
+
     #[test]
     fn test_tok_get_num() {
-        assert_eq!(
-            tok_get_num(&("0".chars().collect::<Vec<char>>()), 0),
-            ("0".to_string(), 1)
-        );
-        assert_eq!(
-            tok_get_num(&("1".chars().collect::<Vec<char>>()), 0),
-            ("1".to_string(), 1)
-        );
-        assert_eq!(
-            tok_get_num(&("34".chars().collect::<Vec<char>>()), 0),
-            ("34".to_string(), 2)
-        );
-        assert_eq!(
-            tok_get_num(&("56a".chars().collect::<Vec<char>>()), 0),
-            ("56".to_string(), 2)
-        );
-        assert_eq!(
-            tok_get_num(&("".chars().collect::<Vec<char>>()), 0),
-            ("".to_string(), 0)
-        );
-        assert_eq!(
-            tok_get_num(&("a".chars().collect::<Vec<char>>()), 0),
-            ("".to_string(), 0)
-        );
+        assert_eq!(tok_get_num(&s2v("0"), 0), ("0".to_string(), 1));
+        assert_eq!(tok_get_num(&s2v("1"), 0), ("1".to_string(), 1));
+        assert_eq!(tok_get_num(&s2v("34"), 0), ("34".to_string(), 2));
+        assert_eq!(tok_get_num(&s2v("56a"), 0), ("56".to_string(), 2));
+        assert_eq!(tok_get_num(&s2v(""), 0), ("".to_string(), 0));
+        assert_eq!(tok_get_num(&s2v("a"), 0), ("".to_string(), 0));
     }
     #[test]
     fn test_tok_num_int() {
-        assert_eq!(
-            tok_num_int(&("0x1".chars().collect::<Vec<char>>()), 1),
-            (Ok(Token::Num(1)), 3)
-        );
-        assert_eq!(
-            tok_num_int(&("0xa".chars().collect::<Vec<char>>()), 1),
-            (Ok(Token::Num(10)), 3)
-        );
-        assert_eq!(
-            tok_num_int(&("0x10".chars().collect::<Vec<char>>()), 1),
-            (Ok(Token::Num(16)), 4)
-        );
-        assert_eq!(
-            tok_num_int(&("0b10".chars().collect::<Vec<char>>()), 1),
-            (Ok(Token::Num(2)), 4)
-        );
-        assert_eq!(
-            tok_num_int(&("0b1_0".chars().collect::<Vec<char>>()), 1),
-            (Ok(Token::Num(2)), 5)
-        );
-        assert_eq!(
-            tok_num_int(&("010".chars().collect::<Vec<char>>()), 1),
-            (Ok(Token::Num(8)), 3)
-        );
+        assert_eq!(tok_num_int(&s2v("0x1"), 1), (Ok(Token::Num(1)), 3));
+        assert_eq!(tok_num_int(&s2v("0xa"), 1), (Ok(Token::Num(10)), 3));
+        assert_eq!(tok_num_int(&s2v("0x10"), 1), (Ok(Token::Num(16)), 4));
+        assert_eq!(tok_num_int(&s2v("0b10"), 1), (Ok(Token::Num(2)), 4));
+        assert_eq!(tok_num_int(&s2v("0b1_0"), 1), (Ok(Token::Num(2)), 5));
+        assert_eq!(tok_num_int(&s2v("010"), 1), (Ok(Token::Num(8)), 3));
     }
     #[test]
     fn test_tok_num() {
+        assert_eq!(tok_num(&s2v("0x1"), 0), (Ok(Token::Num(1)), 3));
+        assert_eq!(tok_num(&s2v("0xa"), 0), (Ok(Token::Num(10)), 3));
+        assert_eq!(tok_num(&s2v("0x10"), 0), (Ok(Token::Num(16)), 4));
+        assert_eq!(tok_num(&s2v("0b10"), 0), (Ok(Token::Num(2)), 4));
+        assert_eq!(tok_num(&s2v("0b1_0"), 0), (Ok(Token::Num(2)), 5));
+        assert_eq!(tok_num(&s2v("010"), 0), (Ok(Token::Num(8)), 3));
+        assert_eq!(tok_num(&s2v("10"), 0), (Ok(Token::Num(10)), 2));
+        assert_eq!(tok_num(&s2v("10.1"), 0), (Ok(Token::FNum(10.1)), 4));
+        assert_eq!(tok_num(&s2v("10e1"), 0), (Ok(Token::FNum(100.0)), 4));
+        assert_eq!(tok_num(&s2v("10e1+"), 0), (Ok(Token::FNum(100.0)), 4));
+        assert_eq!(tok_num(&s2v("1"), 0), (Ok(Token::Num(1)), 1));
+        assert_eq!(tok_num(&s2v("0"), 0), (Ok(Token::Num(0)), 1));
+        assert_eq!(tok_num(&s2v("10"), 0), (Ok(Token::Num(10)), 2));
+        assert_eq!(tok_num(&s2v("1.1"), 0), (Ok(Token::FNum(1.1)), 3));
+        assert_eq!(tok_num(&s2v("0.1"), 0), (Ok(Token::FNum(0.1)), 3));
+        assert_eq!(tok_num(&s2v("1.1E2"), 0), (Ok(Token::FNum(110.0)), 5));
+        assert_eq!(tok_num(&s2v("1.1E-2"), 0), (Ok(Token::FNum(0.011)), 6));
+        assert_eq!(tok_num(&s2v("100_000"), 0), (Ok(Token::Num(100000)), 7));
+        assert_eq!(tok_num(&s2v("0xa"), 0), (Ok(Token::Num(10)), 3));
+        assert_eq!(tok_num(&s2v("011"), 0), (Ok(Token::Num(9)), 3));
+        assert_eq!(tok_num(&s2v("0b11"), 0), (Ok(Token::Num(3)), 4));
+        assert_eq!(tok_num(&s2v("1e3"), 0), (Ok(Token::FNum(1000.0)), 3));
         assert_eq!(
-            tok_num(&("0x1".chars().collect::<Vec<char>>()), 0),
-            (Ok(Token::Num(1)), 3)
-        );
-        assert_eq!(
-            tok_num(&("0xa".chars().collect::<Vec<char>>()), 0),
-            (Ok(Token::Num(10)), 3)
-        );
-        assert_eq!(
-            tok_num(&("0x10".chars().collect::<Vec<char>>()), 0),
-            (Ok(Token::Num(16)), 4)
-        );
-        assert_eq!(
-            tok_num(&("0b10".chars().collect::<Vec<char>>()), 0),
-            (Ok(Token::Num(2)), 4)
-        );
-        assert_eq!(
-            tok_num(&("0b1_0".chars().collect::<Vec<char>>()), 0),
-            (Ok(Token::Num(2)), 5)
-        );
-        assert_eq!(
-            tok_num(&("010".chars().collect::<Vec<char>>()), 0),
-            (Ok(Token::Num(8)), 3)
-        );
-        assert_eq!(
-            tok_num(&("10".chars().collect::<Vec<char>>()), 0),
-            (Ok(Token::Num(10)), 2)
-        );
-        assert_eq!(
-            tok_num(&("10.1".chars().collect::<Vec<char>>()), 0),
-            (Ok(Token::FNum(10.1)), 4)
-        );
-        assert_eq!(
-            tok_num(&("10e1".chars().collect::<Vec<char>>()), 0),
-            (Ok(Token::FNum(100.0)), 4)
-        );
-        assert_eq!(
-            tok_num(&("10e1+".chars().collect::<Vec<char>>()), 0),
-            (Ok(Token::FNum(100.0)), 4)
-        );
-        assert_eq!(
-            tok_num(&("1".chars().collect::<Vec<char>>()), 0),
-            (Ok(Token::Num(1)), 1)
-        );
-        assert_eq!(
-            tok_num(&("1".chars().collect::<Vec<char>>()), 0),
-            (Ok(Token::Num(1)), 1)
-        );
-        assert_eq!(
-            tok_num(&("0".chars().collect::<Vec<char>>()), 0),
-            (Ok(Token::Num(0)), 1)
-        );
-        assert_eq!(
-            tok_num(&("10".chars().collect::<Vec<char>>()), 0),
-            (Ok(Token::Num(10)), 2)
-        );
-        assert_eq!(
-            tok_num(&("1.1".chars().collect::<Vec<char>>()), 0),
-            (Ok(Token::FNum(1.1)), 3)
-        );
-        assert_eq!(
-            tok_num(&("0.1".chars().collect::<Vec<char>>()), 0),
-            (Ok(Token::FNum(0.1)), 3)
-        );
-        assert_eq!(
-            tok_num(&("1.1E2".chars().collect::<Vec<char>>()), 0),
-            (Ok(Token::FNum(110.0)), 5)
-        );
-        assert_eq!(
-            tok_num(&("1.1E-2".chars().collect::<Vec<char>>()), 0),
-            (Ok(Token::FNum(0.011)), 6)
-        );
-        assert_eq!(
-            tok_num(&("100_000".chars().collect::<Vec<char>>()), 0),
-            (Ok(Token::Num(100000)), 7)
-        );
-        assert_eq!(
-            tok_num(&("0xa".chars().collect::<Vec<char>>()), 0),
-            (Ok(Token::Num(10)), 3)
-        );
-        assert_eq!(
-            tok_num(&("011".chars().collect::<Vec<char>>()), 0),
-            (Ok(Token::Num(9)), 3)
-        );
-        assert_eq!(
-            tok_num(&("0b11".chars().collect::<Vec<char>>()), 0),
-            (Ok(Token::Num(3)), 4)
-        );
-        assert_eq!(
-            tok_num(&("1e3".chars().collect::<Vec<char>>()), 0),
-            (Ok(Token::FNum(1000.0)), 3)
-        );
-        assert_eq!(
-            tok_num(&("9223372036854775807".chars().collect::<Vec<char>>()), 0),
+            tok_num(&s2v("9223372036854775807"), 0),
             (Ok(Token::Num(9223372036854775807)), 19)
         );
         assert_eq!(
-            tok_num(&("18446744073709551615".chars().collect::<Vec<char>>()), 0),
+            tok_num(&s2v("18446744073709551615"), 0),
             (Ok(Token::Num(18446744073709551615)), 20)
         );
     }
     #[test]
     fn test_tok_ident() {
         assert_eq!(
-            tok_ident(&("i".chars().collect::<Vec<char>>()), 0),
+            tok_ident(&s2v("i"), 0),
             (Ok(Token::Ident("i".to_string())), 1)
         );
         assert_eq!(
-            tok_ident(&("sin()".chars().collect::<Vec<char>>()), 0),
+            tok_ident(&s2v("sin()"), 0),
             (Ok(Token::Ident("sin".to_string())), 3)
         );
     }
