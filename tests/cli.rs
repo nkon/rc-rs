696e9cd0963606634ed293ac26d9430ed5890337
Kondou,Nobuhiro <kondou.nobuhiro@gmail.com>
2025-03-01 13:42:58 -0600

    add tests/cli.rs for integration test

---
+++ tests/cli.rs
@@ -0,0 +1,37 @@
+use assert_cmd::Command;
+
+#[test]
+fn runs() {
+    let mut cmd = Command::cargo_bin("rc").unwrap();
+    cmd.arg("1").assert().success().stdout("1\n");
+}
+
+#[test]
+fn runs2() {
+    let mut cmd = Command::cargo_bin("rc").unwrap();
+    cmd.arg("1+2*3").assert().success().stdout("7\n");
+}
+
+#[test]
+fn runs3() {
+    let mut cmd = Command::cargo_bin("rc").unwrap();
+    cmd.arg("sin(0)").assert().success().stdout("0\n");
+}
+
+#[test]
+fn runs4() {
+    let mut cmd = Command::cargo_bin("rc").unwrap();
+    cmd.arg("abs(-2)").assert().success().stdout("2\n");
+}
+
+#[test]
+fn runs5() {
+    let mut cmd = Command::cargo_bin("rc").unwrap();
+    cmd.arg("max(1,2,3)").assert().success().stdout("3\n");
+}
+
+#[test]
+fn runs6() {
+    let mut cmd = Command::cargo_bin("rc").unwrap();
+    cmd.arg("1.0/4.0").assert().success().stdout("0.25\n");
+}
